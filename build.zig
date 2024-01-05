f8064663d6565432dee7e570cb5d7e9c873fc73e
Raiden1411 <67233402+Raiden1411@users.noreply.github.com>
2024-01-05 13:55:06 +0000

    chore: add coverage build option and setup initial CI

--- build.zig
+++ build.zig
@@ -1,10 +1,24 @@
 const std = @import("std");
+const builtin = @import("builtin");
+
+const min_zig_string = "0.12.0-dev.1767+1e42a3de89";
 
 pub fn build(b: *std.Build) void {
+    comptime {
+        const current_zig = builtin.zig_version;
+        const min_zig = std.SemanticVersion.parse(min_zig_string) catch unreachable;
+        if (current_zig.order(min_zig) == .lt) {
+            @compileError(std.fmt.comptimePrint("Your Zig version v{} does not meet the minimum build requirement of v{}", .{ current_zig, min_zig }));
+        }
+    }
+
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
     _ = b.addModule("zabi", .{ .source_file = .{ .path = "src/root.zig" } });
+
+    const coverage = b.option(bool, "generate_coverage", "Generate coverage data with kcov") orelse false;
+    const coverage_output_dir = b.option([]const u8, "coverage_output_dir", "Output directory for coverage data") orelse b.pathJoin(&.{ b.install_prefix, "kcov" });
 
     const lib = b.addStaticLibrary(.{
         .name = "zabi",
@@ -32,4 +46,26 @@ pub fn build(b: *std.Build) void {
     // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_lib_unit_tests.step);
+
+    // Coverage build option with kcov
+    if (coverage) {
+        const include = b.fmt("--include-pattern=/src", .{});
+        const args = &[_]std.build.RunStep.Arg{ .{ .bytes = b.dupe("kcov") }, .{ .bytes = b.dupe("--collect-only") }, .{ .bytes = b.dupe(include) }, .{ .bytes = b.dupe(coverage_output_dir) } };
+
+        var tests_run = b.addRunArtifact(lib_unit_tests);
+        tests_run.has_side_effects = true;
+        tests_run.argv.insertSlice(0, args) catch @panic("OutOfMemory");
+
+        var merge = std.build.RunStep.create(b, "merge kcov");
+        merge.has_side_effects = true;
+        merge.addArgs(&.{
+            "kcov",
+            "--merge",
+            coverage_output_dir,
+            b.pathJoin(&.{ coverage_output_dir, "test" }),
+        });
+        merge.step.dependOn(&b.addRemoveDirTree(coverage_output_dir).step);
+        merge.step.dependOn(&tests_run.step);
+        test_step.dependOn(&merge.step);
+    }
 }

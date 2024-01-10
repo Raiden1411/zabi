a941b54bf97ab570ae7e21d3c88861cbeee54f76
Raiden1411 <67233402+Raiden1411@users.noreply.github.com>
2024-01-10 23:47:16 +0000

    feat: add support for transaction receipt and logs methods

---
+++ src/meta/ethereum.zig
@@ -0,0 +1,71 @@
+const std = @import("std");
+const meta = @import("meta.zig");
+
+pub const Hex = []const u8;
+pub const Gwei = u64;
+pub const Wei = u256;
+pub const Ether = f64;
+
+/// Set of public rpc actions.
+pub const EthereumRpcMethods = enum {
+    eth_chainId,
+    eth_gasPrice,
+    eth_accounts,
+    eth_getBalance,
+    eth_getBlockByNumber,
+    eth_getBlockByHash,
+    eth_blockNumber,
+    eth_getTransactionCount,
+    eth_getBlockTransactionCountByHash,
+    eth_getBlockTransactionCountByNumber,
+    eth_getUncleCountByBlockHash,
+    eth_getUncleCountByBlockNumber,
+    eth_getCode,
+    eth_getTransactionByHash,
+    eth_getTransactionByBlockHashAndIndex,
+    eth_getTransactionByBlockNumberAndIndex,
+    eth_getTransactionReceipt,
+    eth_getUncleByBlockHashAndIndex,
+    eth_getUncleByBlockNumberAndIndex,
+    eth_newFilter,
+    eth_newBlockFilter,
+    eth_newPendingTransactionFilter,
+    eth_uninstallFilter,
+    eth_getFilterChanges,
+    eth_getFilterLogs,
+    eth_getLogs,
+    eth_sign,
+    eth_signTransaction,
+    eth_sendTransaction,
+    eth_sendRawTransaction,
+    eth_call,
+    eth_estimateGas,
+};
+
+pub fn EthereumRequest(comptime T: type) type {
+    return struct {
+        jsonrpc: []const u8 = "2.0",
+        method: EthereumRpcMethods,
+        params: T,
+        id: usize,
+    };
+}
+
+pub fn EthereumResponse(comptime T: type) type {
+    return struct {
+        jsonrpc: []const u8,
+        id: usize,
+        result: T,
+
+        pub usingnamespace if (@typeInfo(T) == .Int) meta.RequestParser(@This()) else struct {};
+    };
+}
+
+pub const ErrorResponse = struct {
+    code: isize,
+    message: []const u8,
+};
+
+pub const EthereumErrorResponse = struct { jsonrpc: []const u8, id: usize, @"error": ErrorResponse };
+
+pub const HexRequestParameters = []const Hex;
---
+++ src/meta/log.zig
@@ -0,0 +1,36 @@
+const block = @import("block.zig");
+const meta = @import("meta.zig");
+const types = @import("ethereum.zig");
+
+pub const Log = struct {
+    address: types.Hex,
+    topics: []const types.Hex,
+    blockHash: ?types.Hex,
+    blockNumber: ?u64,
+    data: types.Hex,
+    logIndex: ?usize,
+    transactionHash: ?types.Hex,
+    transactionIndex: ?usize,
+    removed: bool,
+
+    pub usingnamespace meta.RequestParser(@This());
+};
+
+pub const Logs = []const Log;
+
+pub const LogRequest = struct {
+    fromBlock: ?types.Hex = null,
+    toBlock: ?types.Hex = null,
+    address: ?types.Hex = null,
+    topics: ?[]const types.Hex = null,
+    blockHash: ?types.Hex = null,
+};
+
+pub const LogRequestParams = struct {
+    fromBlock: ?u64 = null,
+    toBlock: ?u64 = null,
+    tag: ?block.BalanceBlockTag = null,
+    address: ?types.Hex = null,
+    topics: ?[]const types.Hex = null,
+    blockHash: ?types.Hex = null,
+};

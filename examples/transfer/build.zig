5fe13d9d7d2663ff7430400b4e56b4ef02ea8ee6
Raiden1411 <67233402+Raiden1411@users.noreply.github.com>
2024-03-06 15:58:17 +0000

    feat: add transfer example

---
+++ examples/transfer/build.zig
@@ -0,0 +1,30 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    _ = b.addModule("transfer_example", .{ .root_source_file = .{ .path = "transfer.zig" } });
+
+    const exe = b.addExecutable(.{
+        .name = "transfer_example",
+        .root_source_file = .{ .path = "transfer.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    addDependencies(b, exe);
+    b.installArtifact(exe);
+}
+
+fn addDependencies(b: *std.Build, step: *std.Build.Step.Compile) void {
+    const target = step.root_module.resolved_target.?;
+    const optimize = step.root_module.optimize.?;
+
+    const zabi = b.dependency("zabi", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    step.root_module.addImport("zabi", zabi.module("zabi"));
+}
---
+++ examples/transfer/transfer.zig
@@ -0,0 +1,28 @@
+const std = @import("std");
+const utils = zabi.utils;
+const zabi = @import("zabi");
+
+const Wallet = zabi.wallet.Wallet(.http);
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    var iter = try std.process.ArgIterator.initWithAllocator(gpa.allocator());
+    defer iter.deinit();
+
+    _ = iter.skip();
+
+    const private_key = iter.next().?;
+    const host_url = iter.next().?;
+
+    const uri = try std.Uri.parse(host_url);
+    var wallet = try Wallet.init(private_key, .{ .allocator = gpa.allocator(), .uri = uri, .chain_id = .sepolia, .pooling_interval = 4_000 });
+    defer wallet.deinit();
+
+    const hash = try wallet.sendTransaction(.{ .type = .london, .to = try utils.addressToBytes("0x0000000000000000000000000000000000000000"), .value = 42069 });
+    const receipt = try wallet.waitForTransactionReceipt(hash, 1);
+
+    if (receipt) |tx_receipt| {
+        std.debug.print("Transaction receipt: {}", .{tx_receipt});
+    } else std.process.exit(1);
+}

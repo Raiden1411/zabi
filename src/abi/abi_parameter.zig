39d9c89efb9b0087d81e4f6a4a4b7e779199c258
Raiden1411 <67233402+Raiden1411@users.noreply.github.com>
2024-03-10 18:34:55 +0000

    chore: increase abi_parameter coverage

--- src/abi/abi_parameter.zig
+++ src/abi/abi_parameter.zig
@@ -288,3 +288,55 @@ test "Json parse error" {
 
     try testing.expectError(error.UnknownField, std.json.parseFromSlice(AbiParameter, testing.allocator, sslice, .{}));
 }
+
+test "Prepare" {
+    {
+        const param: AbiParameter = .{ .type = .{ .tuple = {} }, .name = "foo", .components = &.{.{ .type = .{ .address = {} }, .name = "bar" }} };
+
+        var c_writter = std.io.countingWriter(std.io.null_writer);
+        try param.prepare(c_writter.writer());
+
+        const bytes = c_writter.bytes_written;
+        const size = std.math.cast(usize, bytes) orelse return error.OutOfMemory;
+
+        const buffer = try testing.allocator.alloc(u8, size);
+        defer testing.allocator.free(buffer);
+
+        var buf_writter = std.io.fixedBufferStream(buffer);
+        try param.prepare(buf_writter.writer());
+
+        const slice = buf_writter.getWritten();
+
+        try testing.expectEqualStrings(slice, "(address)");
+    }
+    {
+        const param: AbiEventParameter = .{ .type = .{ .tuple = {} }, .name = "foo", .indexed = false, .components = &.{.{ .type = .{ .address = {} }, .name = "bar" }} };
+
+        var c_writter = std.io.countingWriter(std.io.null_writer);
+        try param.prepare(c_writter.writer());
+
+        const bytes = c_writter.bytes_written;
+        const size = std.math.cast(usize, bytes) orelse return error.OutOfMemory;
+
+        const buffer = try testing.allocator.alloc(u8, size);
+        defer testing.allocator.free(buffer);
+
+        var buf_writter = std.io.fixedBufferStream(buffer);
+        try param.prepare(buf_writter.writer());
+
+        const slice = buf_writter.getWritten();
+
+        try testing.expectEqualStrings(slice, "(address)");
+    }
+}
+
+test "Format" {
+    {
+        const param: AbiEventParameter = .{ .type = .{ .tuple = {} }, .name = "foo", .indexed = false, .components = &.{.{ .type = .{ .address = {} }, .name = "bar" }} };
+        try testing.expectFmt("(address bar) foo", "{s}", .{param});
+    }
+    {
+        const param: AbiParameter = .{ .type = .{ .tuple = {} }, .name = "foo", .components = &.{.{ .type = .{ .address = {} }, .name = "bar" }} };
+        try testing.expectFmt("(address bar) foo", "{s}", .{param});
+    }
+}

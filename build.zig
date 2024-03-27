07271456ec65b35689590abe0959d561c6c9fad4
Raiden1411 <67233402+Raiden1411@users.noreply.github.com>
2024-03-27 15:10:26 +0000

    chore: readd coverage report

--- build.zig
+++ build.zig
@@ -44,7 +44,7 @@ pub fn build(b: *std.Build) void {
     // Coverage build option with kcov
     if (coverage) {
         const include = b.fmt("--include-pattern=/src", .{});
-        const args = &[_]std.Build.Step.Run.Arg{ .{ .bytes = b.dupe("kcov") }, .{ .bytes = b.dupe("--collect-only") }, .{ .bytes = b.dupe(include) }, .{ .bytes = b.dupe(coverage_output_dir) } };
+        const args = &[_]std.Build.Step.Run.Arg{ .{ .bytes = b.dupe("kcov") }, .{ .bytes = b.dupe(include) }, .{ .bytes = b.dupe(coverage_output_dir) } };
 
         var tests_run = b.addRunArtifact(lib_unit_tests);
         run_lib_unit_tests.has_side_effects = true;

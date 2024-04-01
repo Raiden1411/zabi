93a1b176b77b638b517990032bf3fc7e28f8d828
Raiden1411 <67233402+Raiden1411@users.noreply.github.com>
2024-04-01 10:07:06 +0100

    ci: test

--- pkg/blst/build.zig
+++ pkg/blst/build.zig
@@ -15,7 +15,7 @@ pub fn build(b: *std.Build) !void {
 }
 
 fn buildBlst(b: *std.Build, upstream: *std.Build.Dependency, target: std.Build.ResolvedTarget, optimize: std.builtin.OptimizeMode) !*std.Build.Step.Compile {
-    const lib = b.addSharedLibrary(.{ .name = "blst", .target = target, .optimize = optimize });
+    const lib = b.addStaticLibrary(.{ .name = "blst", .target = target, .optimize = optimize });
 
     lib.addIncludePath(upstream.path("src"));
     lib.addIncludePath(upstream.path("build"));
@@ -23,7 +23,9 @@ fn buildBlst(b: *std.Build, upstream: *std.Build.Dependency, target: std.Build.R
     var flags = std.ArrayList([]const u8).init(b.allocator);
     defer flags.deinit();
 
-    try flags.appendSlice(&.{"-D__BLST_PORTABLE__"});
+    if (!target.result.isDarwin()) {
+        try flags.appendSlice(&.{"-D__BLST_PORTABLE__"});
+    }
 
     lib.addCSourceFiles(.{ .root = upstream.path(""), .flags = flags.items, .files = &.{ "src/server.c", "build/assembly.S" } });
 

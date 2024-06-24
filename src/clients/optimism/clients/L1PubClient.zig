f5db706e420affaa0596e3cfa0fc32a8898723bc
Raiden1411 <67233402+Raiden1411@users.noreply.github.com>
2024-06-24 16:16:04 +0100

    build: update rand for op l1 client

--- src/clients/optimism/clients/L1PubClient.zig
+++ src/clients/optimism/clients/L1PubClient.zig
@@ -103,7 +103,7 @@ pub fn L1Client(comptime client_type: Clients) type {
             const games = try self.getGames(limit, block_number);
             defer self.allocator.free(games);
 
-            var rand = std.rand.DefaultPrng.init(@intCast(block_number * limit));
+            var rand = std.Random.DefaultPrng.init(@intCast(block_number * limit));
 
             if (games.len == 0)
                 return error.GameNotFound;

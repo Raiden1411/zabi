090cf681451df204dd05207f3d66aa0b3deafc33
Raiden1411 <67233402+Raiden1411@users.noreply.github.com>
2024-08-30 19:04:25 +0100

    tests: update op contracts location

--- src/tests/constants.zig
+++ src/tests/constants.zig
@@ -16,11 +16,13 @@ pub const anvil_op_sepolia: NetworkConfig = .{
     .chain_id = .op_sepolia,
     .op_stack_contracts = .{
         .portalAddress = utils.addressToBytes("0x16Fc5058F25648194471939df75CF27A2fdC48BC") catch unreachable,
-        .disputeGameFactory = utils.addressToBytes("0x05F9613aDB30026FFd634f38e5C4dFd30a197Fa1") catch unreachable,
     },
 };
 
 pub const anvil_sepolia: NetworkConfig = .{
     .endpoint = .{ .uri = Uri.parse("http://localhost:6971/") catch unreachable },
     .chain_id = .sepolia,
+    .op_stack_contracts = .{
+        .disputeGameFactory = utils.addressToBytes("0x05F9613aDB30026FFd634f38e5C4dFd30a197Fa1") catch unreachable,
+    },
 };

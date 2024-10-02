52479f3d6c669daa208e93a475caf8f3e2f90dd1
Raiden1411 <67233402+Raiden1411@users.noreply.github.com>
2024-10-02 21:13:13 +0100

    examples: update allocator

--- examples/interpreter/interpreter.zig
+++ examples/interpreter/interpreter.zig
@@ -12,25 +12,24 @@ pub const CliOptions = struct {
 };
 
 pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer _ = gpa.deinit();
+    const allocator = std.heap.c_allocator;
 
-    var iter = try std.process.argsWithAllocator(gpa.allocator());
+    var iter = try std.process.argsWithAllocator(allocator);
     defer iter.deinit();
 
-    const parsed = args_parser.parseArgs(CliOptions, gpa.allocator(), &iter);
+    const parsed = args_parser.parseArgs(CliOptions, allocator, &iter);
 
-    const buffer = try gpa.allocator().alloc(u8, @divExact(parsed.bytecode.len, 2));
-    defer gpa.allocator().free(buffer);
+    const buffer = try allocator.alloc(u8, @divExact(parsed.bytecode.len, 2));
+    defer allocator.free(buffer);
 
-    const calldata = try gpa.allocator().alloc(u8, @divExact(parsed.calldata.len, 2));
-    defer gpa.allocator().free(calldata);
+    const calldata = try allocator.alloc(u8, @divExact(parsed.calldata.len, 2));
+    defer allocator.free(calldata);
 
     _ = try std.fmt.hexToBytes(buffer, parsed.bytecode);
     _ = try std.fmt.hexToBytes(calldata, parsed.calldata);
 
     const contract_instance = try Contract.init(
-        gpa.allocator(),
+        allocator,
         calldata,
         .{ .raw = buffer },
         null,
@@ -38,20 +37,20 @@ pub fn main() !void {
         [_]u8{1} ** 20,
         [_]u8{0} ** 20,
     );
-    defer contract_instance.deinit(gpa.allocator());
+    defer contract_instance.deinit(allocator);
 
     var plain: PlainHost = undefined;
     defer plain.deinit();
 
-    plain.init(gpa.allocator());
+    plain.init(allocator);
 
     var interpreter: Interpreter = undefined;
     defer interpreter.deinit();
 
-    try interpreter.init(gpa.allocator(), contract_instance, plain.host(), .{ .gas_limit = 300_000_000 });
+    try interpreter.init(allocator, contract_instance, plain.host(), .{ .gas_limit = 300_000_000 });
 
     const result = try interpreter.run();
-    defer result.deinit(gpa.allocator());
+    defer result.deinit(allocator);
 
     std.debug.print("Interpreter result: {any}", .{result});
 }

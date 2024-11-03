6981bb2a283159eefdf57c6bd380e46b697cd040
Raiden1411 <67233402+Raiden1411@users.noreply.github.com>
2024-11-03 18:34:58 +0000

    crypto: remove normalization from the scope

--- src/crypto/mnemonic.zig
+++ src/crypto/mnemonic.zig
@@ -6,13 +6,10 @@ const testing = std.testing;
 const utils = @import("zabi-utils").utils;
 
 const Allocator = std.mem.Allocator;
-const ArenaAllocator = std.heap.ArenaAllocator;
 const HmacSha512 = std.crypto.auth.hmac.sha2.HmacSha512;
 const OutputTooLongError = errors.OutputTooLongError;
 const Sha256 = std.crypto.hash.sha2.Sha256;
 const WeakParametersError = errors.WeakParametersError;
-const NormData = Normalize.NormData;
-const Normalize = @import("Normalize");
 
 /// Wordlist of valid english mnemonic words.
 /// Currently only english can be loaded at comptime since
@@ -70,58 +67,6 @@ pub fn toEntropy(
     var count: usize = 0;
     while (iter.next()) |word| {
         const index = words.getIndex(word) orelse return error.InvalidMnemonicWord;
-
-        for (0..11) |bit| {
-            if (index & std.math.shl(u16, 1, 10 - bit) != 0) {
-                entropy[count >> 3] |= std.math.shl(u8, 1, 7 - (count % 8));
-            }
-            count += 1;
-        }
-    }
-
-    const entropy_bytes = comptime 32 * word_count / 3;
-    const checksum_bytes = comptime word_count / 3;
-    const checksum_mask = ((1 << checksum_bytes) - 1) << (8 - checksum_bytes) & 0xFF;
-
-    var buffer: [32]u8 = undefined;
-    Sha256.hash(entropy[0 .. entropy_bytes / 8], &buffer, .{});
-
-    const checksum = buffer[0] & checksum_mask;
-
-    if (checksum != entropy[entropy.len - 1] & checksum)
-        return error.InvalidMnemonicChecksum;
-
-    return entropy[0 .. entropy_bytes / 8].*;
-}
-/// Converts the mnemonic phrase into it's entropy representation.
-///
-/// This will normalize the words in the mnemonic phrase.
-///
-/// **Example**
-/// ```zig
-/// const seed = "test test test test test test test test test test test junk";
-/// const entropy = try toEntropyNormalize(12, seed, null);
-///
-/// const bar = try fromEntropy(testing.allocator, 12, entropy, null);
-/// defer testing.allocator.free(bar);
-///
-/// try testing.expectEqualStrings(seed, bar);
-/// ```
-pub fn toEntropyNormalize(
-    comptime word_count: comptime_int,
-    password: []const u8,
-    wordlist: ?Wordlist,
-) !EntropyArray(word_count) {
-    const words = wordlist orelse english;
-
-    var iter = std.mem.tokenizeScalar(u8, password, ' ');
-    const size = comptime std.math.divCeil(u16, 11 * word_count, 8) catch @compileError("Invalid word_count size");
-
-    var entropy: [size]u8 = [_]u8{0} ** size;
-
-    var count: usize = 0;
-    while (iter.next()) |word| {
-        const index = try words.getIndexAndNormalize(word) orelse return error.InvalidMnemonicWord;
 
         for (0..11) |bit| {
             if (index & std.math.shl(u16, 1, 10 - bit) != 0) {
@@ -223,51 +168,18 @@ pub const Wordlist = struct {
 
     /// List of 2048 size of words.
     word_list: [Wordlist.list_count][]const u8,
-    /// Allocator used for normalization
-    arena: ?*ArenaAllocator = null,
-
-    /// Frees the slices from the list if the list was
-    /// previously normalized
-    pub fn deinit(self: List) void {
-        if (self.arena) |arena| {
-            const child = arena.child_allocator;
-            arena.deinit();
-
-            child.destroy(arena);
-        }
-    }
     /// Loads word in it's raw format and parses it.
     /// It expects that the string is seperated by "\n"
     ///
-    /// If your list needs to be normalized consider using `loadListAndNormalize`
+    /// This will not normalize the list. Make sure that is done beforehand.
     pub fn loadRawList(raw_list: []const u8) List {
         return .{ .word_list = loadList(raw_list) };
-    }
-    /// Loads word in it's raw format and parses it.
-    /// It expects that the string is seperated by "\n"
-    ///
-    /// It normalizes the words in the list and the returned `List`
-    /// must be deallocated.
-    ///
-    /// You can find the lists [here](https://github.com/bitcoin/bips/blob/master/bip-0039/bip-0039-wordlists.md)
-    pub fn loadListAndNormalize(allocator: Allocator, raw_list: []const u8) !List {
-        const arena = try allocator.create(ArenaAllocator);
-        errdefer allocator.destroy(arena);
-
-        arena.* = ArenaAllocator.init(allocator);
-        errdefer arena.deinit();
-
-        return .{
-            .arena = arena,
-            .word_list = try loadAndNormalize(arena.allocator(), raw_list),
-        };
     }
     /// Performs binary search on the word list
     /// as we assume that the list is alphabetically ordered.
     ///
     /// Returns null if the word isn't on the list
-    ///
-    /// If you need to normalize the word first consider using `getIndexAndNormalize`.
+    /// This methods doesn't normalize the `word`. Make sure that it's normalized beforehand.
     pub fn getIndex(self: List, word: []const u8) ?u16 {
         if (word.len == 0)
             return null;
@@ -289,30 +201,6 @@ pub const Wordlist = struct {
 
         return null;
     }
-    /// Performs binary search on the word list
-    /// as we assume that the list is alphabetically ordered.
-    ///
-    /// This will also normalize the provided slice.
-    ///
-    /// Returns null if the word isn't on the list
-    pub fn getIndexAndNormalize(self: List, word: []const u8) !?u16 {
-        const allocator = alloc: {
-            const arena = self.arena orelse break :alloc std.heap.page_allocator;
-
-            break :alloc arena.allocator();
-        };
-
-        var data: NormData = .{};
-        defer data.deinit();
-
-        try data.init(allocator);
-        var norm: Normalize = .{ .norm_data = &data };
-
-        const result = try norm.nfkd(allocator, word);
-        defer result.deinit();
-
-        return self.getIndex(result.slice);
-    }
 
     fn loadList(raw_list: []const u8) [Wordlist.list_count][]const u8 {
         @setEvalBranchQuota(40_000);
@@ -323,27 +211,6 @@ pub const Wordlist = struct {
         var count: usize = 0;
         while (iter.next()) |word| {
             list_buffer[count] = word;
-            count += 1;
-        }
-
-        return list_buffer;
-    }
-
-    fn loadAndNormalize(allocator: Allocator, raw_list: []const u8) ![Wordlist.list_count][]const u8 {
-        var data: NormData = .{};
-        defer data.deinit();
-
-        try data.init(allocator);
-
-        var norm: Normalize = .{ .norm_data = &data };
-        var iter = std.mem.tokenizeScalar(u8, raw_list, '\n');
-
-        var list_buffer: [Wordlist.list_count][]const u8 = undefined;
-        var count: usize = 0;
-
-        while (iter.next()) |word| {
-            const result = try norm.nfkd(allocator, word);
-            list_buffer[count] = result.slice;
             count += 1;
         }
 

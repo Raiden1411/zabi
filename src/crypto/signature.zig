568bcd1ec4bbedfeb45c4391022c7110c3070cea
Raiden1411 <67233402+Raiden1411@users.noreply.github.com>
2024-11-13 13:34:33 +0000

    crypto: add schnorr signatures representation

--- src/crypto/signature.zig
+++ src/crypto/signature.zig
@@ -3,6 +3,52 @@ const std = @import("std");
 const Allocator = std.mem.Allocator;
 const Secp256k1 = std.crypto.ecc.Secp256k1;
 
+/// Zig representation of an bip0340 schnorr signature.
+pub const SchnorrSignature = struct {
+    r: [32]u8,
+    s: [32]u8,
+
+    /// Converts the struct signature into bytes.
+    pub fn toBytes(sig: SchnorrSignature) [64]u8 {
+        var signed: [64]u8 = undefined;
+
+        @memcpy(signed[0..32], sig.r[0..]);
+        @memcpy(signed[32..64], sig.s[0..]);
+
+        return signed;
+    }
+    /// Converts the signature bytes into the struct.
+    pub fn fromBytes(sig: [64]u8) SchnorrSignature {
+        return .{
+            .r = sig[0..32].*,
+            .s = sig[32..64].*,
+        };
+    }
+    /// Converts the struct signature into a hex string.
+    ///
+    /// Caller owns the memory
+    pub fn toHex(sig: SchnorrSignature, allocator: Allocator) Allocator.Error![]u8 {
+        const bytes = sig.toBytes();
+
+        return std.fmt.allocPrint(allocator, "{s}", .{std.fmt.fmtSliceHexLower(bytes[0..])});
+    }
+    /// Converts a hex signature into it's struct representation.
+    pub fn fromHex(hex: []const u8) error{ NoSpaceLeft, InvalidSignature, InvalidLength, InvalidCharacter }!SchnorrSignature {
+        const signature = if (std.mem.startsWith(u8, hex, "0x")) hex[2..] else hex;
+
+        if (signature.len != 128)
+            return error.InvalidSignature;
+
+        var signed: [64]u8 = undefined;
+        _ = try std.fmt.hexToBytes(signed[0..], signature);
+
+        return .{
+            .r = signed[0..32].*,
+            .s = signed[32..64].*,
+        };
+    }
+};
+
 /// Zig representation of an ethereum signature.
 pub const Signature = struct {
     r: u256,
@@ -32,16 +78,18 @@ pub const Signature = struct {
     /// Converts the struct signature into a hex string.
     ///
     /// Caller owns the memory
-    pub fn toHex(sig: Signature, allocator: Allocator) ![]u8 {
+    pub fn toHex(sig: Signature, allocator: Allocator) Allocator.Error![]u8 {
         const bytes = sig.toBytes();
 
         return std.fmt.allocPrint(allocator, "{s}", .{std.fmt.fmtSliceHexLower(bytes[0..])});
     }
     /// Converts a hex signature into it's struct representation.
-    pub fn fromHex(hex: []const u8) !Signature {
+    pub fn fromHex(hex: []const u8) error{ NoSpaceLeft, InvalidSignature, InvalidLength, InvalidCharacter }!Signature {
         const signature = if (std.mem.startsWith(u8, hex, "0x")) hex[2..] else hex;
 
-        if (signature.len != 130) return error.InvalidSignature;
+        if (signature.len != 130)
+            return error.InvalidSignature;
+
         var signed: [65]u8 = undefined;
         _ = try std.fmt.hexToBytes(signed[0..], signature);
 
@@ -50,6 +98,7 @@ pub const Signature = struct {
 
             if (signed[64] == 27) break :rec_id 0 else break :rec_id 1;
         };
+
         return .{
             .r = std.mem.readInt(u256, signed[0..32], .big),
             .s = std.mem.readInt(u256, signed[32..64], .big),
@@ -72,9 +121,8 @@ pub const CompactSignature = struct {
         var bytes: [Secp256k1.scalar.encoded_length]u8 = undefined;
         std.mem.writeInt(u256, &bytes, sig.s, .big);
 
-        if (sig.v == 1) {
+        if (sig.v == 1)
             bytes[0] |= 0x80;
-        }
 
         compact.yParityWithS = std.mem.readInt(u256, &bytes, .big);
 
@@ -91,16 +139,18 @@ pub const CompactSignature = struct {
     /// Converts the struct signature into a hex string.
     ///
     /// Caller owns the memory
-    pub fn toHex(sig: CompactSignature, allocator: Allocator) ![]u8 {
+    pub fn toHex(sig: CompactSignature, allocator: Allocator) Allocator.Error![]u8 {
         const bytes = sig.toBytes();
 
         return std.fmt.allocPrint(allocator, "{s}", .{std.fmt.fmtSliceHexLower(bytes[0..])});
     }
     /// Converts a hex signature into it's struct representation.
-    pub fn fromHex(hex: []const u8) CompactSignature {
+    pub fn fromHex(hex: []const u8) error{ NoSpaceLeft, InvalidSignature, InvalidLength, InvalidCharacter }!CompactSignature {
         const signature = if (std.mem.startsWith(u8, hex, "0x")) hex[2..] else hex;
 
-        if (signature.len != 128) return error.InvalidSignature;
+        if (signature.len != 128)
+            return error.InvalidSignature;
+
         var signed: [64]u8 = undefined;
         _ = try std.fmt.hexToBytes(signed[0..], signature);
 

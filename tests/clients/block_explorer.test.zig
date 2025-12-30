3b47bdb82ba0ae2b4320bae6078e794c1dbde25a
Raiden1411 <67233402+Raiden1411@users.noreply.github.com>
2025-12-30 18:33:16 +0000

    chore: make aarch64 work properly

--- tests/clients/block_explorer.test.zig
+++ tests/clients/block_explorer.test.zig
@@ -5,9 +5,5 @@ const Explorer = @import("zabi").clients.BlockExplorer;
 const QueryParameters = Explorer.QueryParameters;
 
 test "All Ref Decls" {
-    // TODO: Readd this once arm64 llvm bugs have been fixed
-    if (@import("builtin").cpu.arch.isAARCH64())
-        return error.SkipZigTest;
-
     std.testing.refAllDecls(Explorer);
 }
--- tests/utils/utils.test.zig
+++ tests/utils/utils.test.zig
@@ -34,3 +34,31 @@ test "BytesToInt" {
 
     try testing.expectEqual(a, b);
 }
+
+test "FloatToInt" {
+    try testing.expectEqual(utils.floatFromInt(f64, 1), @as(f64, @floatFromInt(1)));
+    try testing.expectEqual(utils.floatFromInt(f64, std.math.maxInt(u8)), @as(f64, @floatFromInt(std.math.maxInt(u8))));
+    try testing.expectEqual(utils.floatFromInt(f64, std.math.maxInt(u16)), @as(f64, @floatFromInt(std.math.maxInt(u16))));
+    try testing.expectEqual(utils.floatFromInt(f64, std.math.maxInt(u32)), @as(f64, @floatFromInt(std.math.maxInt(u32))));
+    try testing.expectEqual(utils.floatFromInt(f64, std.math.maxInt(u64)), @as(f64, @floatFromInt(std.math.maxInt(u64))));
+
+    try testing.expectEqual(utils.floatFromInt(f64, std.math.maxInt(u128)), @as(f64, @floatFromInt(std.math.maxInt(u128))));
+    try testing.expectEqual(utils.floatFromInt(f64, std.math.maxInt(u129)), @as(f64, @floatFromInt(std.math.maxInt(u129))));
+    try testing.expectEqual(utils.floatFromInt(f64, std.math.maxInt(u256)), @as(f64, @floatFromInt(std.math.maxInt(u256))));
+    try testing.expectEqual(utils.floatFromInt(f64, std.math.maxInt(u512)), @as(f64, @floatFromInt(std.math.maxInt(u512))));
+}
+
+test "IntToFloat" {
+    try testing.expectEqual(utils.intFromFloat(u8, 1.0), @as(u8, @intFromFloat(1.0)));
+    try testing.expectEqual(utils.intFromFloat(i8, -1.0), @as(i8, @intFromFloat(-1.0)));
+    try testing.expectEqual(utils.intFromFloat(u16, 1000.1234), @as(u16, @intFromFloat(1000.1234)));
+    try testing.expectEqual(utils.intFromFloat(i16, std.math.minInt(i16)), @as(i16, @intFromFloat(std.math.minInt(i16))));
+    try testing.expectEqual(utils.intFromFloat(u64, 10000000000.1234), @as(u64, @intFromFloat(10000000000.1234)));
+    try testing.expectEqual(utils.intFromFloat(i64, std.math.minInt(i64)), @as(i64, @intFromFloat(std.math.minInt(i64))));
+    try testing.expectEqual(utils.intFromFloat(u128, 1000000000000000000.1234), @as(u128, @intFromFloat(1000000000000000000.1234)));
+    try testing.expectEqual(utils.intFromFloat(i128, std.math.minInt(i128) + 1.000001), @as(i128, @intFromFloat(std.math.minInt(i128) + 1.000001)));
+    try testing.expectEqual(utils.intFromFloat(u256, 1000000000000000000000000000000000000.1234), @as(u256, @intFromFloat(1000000000000000000000000000000000000.1234)));
+    try testing.expectEqual(utils.intFromFloat(i256, std.math.minInt(i256) + 1.000001), @as(i256, @intFromFloat(std.math.minInt(i256) + 1.000001)));
+    try testing.expectEqual(utils.intFromFloat(u512, 1000000000000000000000000000000000000000000000000000000000000000.1234), @as(u512, @intFromFloat(1000000000000000000000000000000000000000000000000000000000000000.1234)));
+    try testing.expectEqual(utils.intFromFloat(i512, std.math.minInt(i512) + 1.000001), @as(i512, @intFromFloat(std.math.minInt(i512) + 1.000001)));
+}
